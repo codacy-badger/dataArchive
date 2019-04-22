a42ca7f84d8fafe8fd89fbbcd6b601e0ed0c9d41
Joli Holmes <jh111@ad.rice.edu>
2019-04-22 11:14:44 -0500

    test getting the commit hash

--- auto_commit.bat
+++ auto_commit.bat
@@ -1,13 +1,29 @@
 @echo off
 
-:: Print the active branch
+:: Return the current branch
+for /f %%i in ('git rev-parse --abbrev-ref HEAD') do set curr_branch=%%i
 
-b=git branch
-if not $b == "* master" (
-    echo Not on master
-) ELSE (
-    echo On Master
-)
+:: Check to see if the branch is on master
+REM if  "%curr_branch%" == "master" (
+REM    echo On master
+REM ) ELSE (
+REM    :again 
+REM       echo Branch is not on master.
+REM       set /p answer="Do you want to create it now (Y/N)?"
+REM       if /i "%answer:~,1%" EQU "Y" echo ""
+REM       if /i "%answer:~,1%" EQU "N" exit /b
+REM       echo Please type Y for Yes or N for No
+REM       goto again
+
+REM )
+
+:: Commit and Push
 git add .
 git commit -m %1
 git push
+
+:: 
+for /f %%i in ('git rev-parse HEAD') do set curr_commit=%%i
+
+echo current commit: "%curr_commit%"
+echo commit message: %1
